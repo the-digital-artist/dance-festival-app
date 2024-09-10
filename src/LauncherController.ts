c69e37f0739e12f0882bf316d156d4969b0e1fe1
Philipp Fischer <fischer.philipp@gmail.com>
2024-09-10 17:02:10 +0200

    Adding check about new Fabric UI Manager (for new architecture)

--- src/LauncherController.ts
+++ src/LauncherController.ts
@@ -261,6 +261,9 @@ class LauncherController extends OperatorStates {
 
     async initialize() {
         const dataModel = DataModel.getInstance().static;
+        const uiManager = global?.nativeFabricUIManager ? 'Fabric' : 'Paper';
+        console.log(`LauncherController - using ${uiManager}`);
+
         try {
             await this.checkForUpdate();
             await Font.loadAsync(this.customFonts);
